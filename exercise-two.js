f110446be31db54c1534caf5c052bf31629af6ce
Abhishek1411287 <stripathiab11@gmail.com>
2025-05-11 21:31:53 -0700

    Add files via upload

---
+++ exercise-two.js
@@ -0,0 +1,8 @@
+ // Sentence with at least five words and a period
+let sentence = "Apples and bananas are tasty.";
+
+// Convert the sentence into an array (splitting by space)
+let wordsArray = sentence.split(" ");
+
+// Log the third word (index 2 since arrays are 0-indexed)
+console.log(wordsArray[2]);  // Output: bananas
